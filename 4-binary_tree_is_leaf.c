f57af357499e413a27f7c170ad7f6842ca7e1f54
melandres8 <melanmosta@gmail.com>
2020-07-21 05:57:56 +0000

    fixing

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node->left && !node->right)
-		return (1);
-	return (0);
+	if (node == NULL)
+		return ();
+	return (!node->left && node->right);
 }
